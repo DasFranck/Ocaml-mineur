610a112925c078833d59c6d89e080ffa53520b35
dasfRArCK <dasfranck@gmx.com>
2014-10-05 06:15:36 +0200

    Modification de tp1 pour ajouter Sierpinski

--- Tp1.ml
+++ Tp1.ml
@@ -1,7 +1,7 @@
 #load "graphics.cma" ;;
 open Graphics;;
 open_graph "" ;;
-(*qqqqq/*)
+
 open Random ;;
 self_init() ;;
 
@@ -36,3 +36,18 @@ let rec dragon n x y z t =
                    dragon (n-1) x y u v;
                    dragon (n-1) z t u v;
                  end ;;
+
+let rec sierpinski n x y z t =
+    match n with
+        |n when n = 0 -> print_string
+        |n when n > 0-> begin
+                moveto x y;
+                lineto z t;
+                moveto x y;
+                lineto (((z-x)/2)+x) (int_of_float(float_of_int(z-x)*.((sqrt 3.)/.2.))+ y);
+                moveto z t;
+                lineto (((z-x)/2)+x) (int_of_float(float_of_int(z-x)*.((sqrt 3.)/.2.))+ y);
+                sierpinski (n-1) x y ((x+z)/2) t;
+                sierpinski (n-1) ((x+z)/2) y z t;
+                sierpinski (n-1) (((z-x)/4)+x) (int_of_float((float_of_int(z-x)*.((sqrt 3.)/.2.))/.2.)+ y) (((z-x)*3/4)+ x) (int_of_float((float_of_int(z-x)*.((sqrt 3.)/.2.))/.2.)+ y) ;
+              end ;;
