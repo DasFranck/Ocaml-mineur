88ae6fb94743d240b3f5a274dd69e8e9e4294b2b
DasArch <dasfranck@gmx.com>
2014-10-12 14:32:51 +0000

    Ajout de l'exo 5 FINI!

--- Tp3/ex5.ml
+++ Tp3/ex5.ml
@@ -1,4 +1,11 @@
 (* Prerequis *)
+let rec reverse a =
+    let rec rec_reverse a b = match a with
+      |[]   -> b
+      |e::l -> rec_reverse l (e::b)
+    in
+        rec_reverse a [];;
+
 let rec append x y = match x with
   |[]    -> y
   |e::l  -> e::(append l y);;
@@ -31,9 +38,41 @@ let morse_to_alphanum x = match x with
   | ['-';'.';'-';'-']    -> 'Y'
   | ['-';'-';'.';'.']    -> 'X'
   | ['/']                -> ' '
-  | _                    -> [] 
+  | []                   -> '%'
+  | _                    -> '#' 
 
 (* 5. decodez moi tout ca! *)
-let morse_to_latin mrs :
-  let len = String.length mrs in
-    let rec 
+let morse_to_latin mrs =
+  let len = (String.length mrs) in
+  let mrs = mrs^" " in
+    let rec mrstolst mrs l n = match n with
+      |n when n<0             -> l 
+      |n when (mrs.[n] = ' ') -> mrstolst mrs ('#'::l) (n-1)
+      |_                      -> mrstolst mrs (mrs.[n]::l) (n-1)
+    in
+   
+    let rec lstto2lst lst = match lst with
+      |[]                           -> lst
+      |e::l when e = '#' || e = '/' -> [] 
+      |e::l                         -> append [e] (lstto2lst l)
+    in
+     
+    let rec dlo lst mst nst = match lst with
+      |[]                   -> nst
+      |e::l when e = '#'    -> dlo l [] (append [lstto2lst mst] nst)
+      |a::e::l when e = '/' -> dlo l [] (append (append [['/']] [lstto2lst (append mst [a])]) nst)
+      |e::l                 -> dlo l (append mst [e]) nst
+    in
+
+    let rec morlst l = match l with
+      |[]   -> ""
+      |m::l -> (String.make 1 (morse_to_alphanum m))^(morlst l)
+    in
+
+    let rec clean lst l n = match n with
+      |n when n<0                               -> l
+      |n when (lst.[n] = '%')                   -> clean lst l (n-1)
+      |n when n = 1 -> clean lst l (n-1)
+      |_                                        -> clean lst (l^(String.make 1 lst.[n])) (n-1)
+
+    in clean (morlst (dlo (mrstolst mrs [] len) [] [[]])) "" (String.length (morlst (dlo (mrstolst mrs [] len) [] [[]])) - 1) ;;
