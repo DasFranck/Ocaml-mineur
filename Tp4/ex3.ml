e7950b202291f4ddfefb8203ec7c9c965f6080b8
DasWin7 <dasfranck@gmx.com>
2014-10-17 05:28:12 +0200

    update ex3

--- Tp4/ex3.ml
+++ Tp4/ex3.ml
@@ -6,11 +6,10 @@ let rec append x y = match x with
   |[]   -> y
   |e::l -> e::(append l y);; 
 
-
 (* 3.1 Cellule *)
 let get_cell (x,y) board = 
   let rec gc1 x n board = match board with
-    |[]              -> failwith "Erreur, liste vide"
+    |[]              -> failwith "Erreur, liste vide (1)"
     |e::l when n = x -> e
     |e::l            -> gc1 x (n+1) l
   in
@@ -26,17 +25,17 @@ let get_cell (x,y) board =
 (* 3.2 Remplacement *)
 let replace_cell value (x,y) board = 
   let rec grcy (y,brd) = match (y,brd) with
-   |(_,[])             -> []
-   |(1,e::l) when y = 1 -> value::l
-   |(n,e::l) when y = n -> e::(grcy ((n-1),l))
-   |_                   -> failwith "Error (2)"
+    |(_,[])             -> []
+    |(1,e::l) when y = 1 -> value::l
+    |(n,e::l) when y = n -> e::(grcy ((n-1),l))
+    |_                   -> failwith "Error (1)"
   in
 
   let rec grcx (x,brd) = match (x,brd) with
-   |(_, [])             -> []
-   |(1,e::l) when x = 1 -> (grcy (y,e))::l
-   |(n,e::l) when x = n -> e::(grcx ((n-1),l))
-   |_                   -> failwith "Error (2)" 
+    |(_, [])             -> []
+    |(1,e::l) when x = 1 -> (grcy (y,e))::l
+    |(n,e::l) when x = n -> e::(grcx ((n-1),l))
+    |_                   -> failwith "Error (2)" 
   in 
      grcx (x,board);;
 
@@ -51,12 +50,18 @@ let seed_life board n =
 
   let rec sl n c board =
     let x = ((Random.int c)+1) and
-    let y = ((Random.int c)+1) in 
+        y = ((Random.int c)+1) in 
     if n > 0 then
       if (get_cell (x,y) board) = 0
-        then sl (n+1) c (replace_cell 1 (x,y) board)
+        then sl (n-1) c (replace_cell 1 (x,y) board)
       else   sl  n    c  board
     else board
 
  in sl n (sizefac board 0) board;;
-(* 3.4 *)
+
+
+(* 3.4 Voisinage *)
+let get_cell_neighborhood (x,y) board =
+  [(get_cell (x-1,y+1) board);(get_cell (x  ,y+1) board);(get_cell (x+1,y+1) board);
+   (get_cell (x-1,y  ) board)                           ;(get_cell (x+1,y  ) board);
+   (get_cell (x-1,y-1) board);(get_cell (x  ,y-1) board);(get_cell (x+1,y-1) board)];;
